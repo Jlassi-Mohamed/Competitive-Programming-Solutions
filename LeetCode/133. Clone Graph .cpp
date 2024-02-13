622d35e08e4bd7ef347f9298d6335e7e0b262057
Jlassi-Mohamed <jlassi.mohamedhani@gmail.com>
2024-02-13 15:11:22 +0100

    Clone Graph solution modified

--- LeetCode/133. Clone Graph .cpp
+++ LeetCode/133. Clone Graph .cpp
@@ -1,9 +1,28 @@
 //
 // Created by jlassimohamed on 2/13/24.
 //
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
 class Solution {
 public:
-
     void dfs(Node* node, Node* m[]){
 
         m[node->val] = new Node(node->val);
