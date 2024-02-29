cc7454392beead4d2aee695181989296be1dc22d
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2024-02-29 11:32:44 +0100

    Create A. Make it White.cpp

---
+++ Codeforces/A. Make it White.cpp
@@ -0,0 +1,69 @@
+    //
+    // Created by jlassimohamed on 2/6/24.
+    //
+    #include <algorithm>
+    #include <array>
+    #include <bitset>
+    #include <cassert>
+    #include <chrono>
+    #include <climits>
+    #include <list>
+    #include <cmath>
+    #include <complex>
+    #include <cstring>
+    #include <functional>
+    #include <iomanip>
+    #include <iostream>
+    #include <map>
+    #include <numeric>
+    #include <queue>
+    #include <random>
+    #include <set>
+    #include <unordered_set>
+    #include <vector>
+    #include <cstring>
+    #define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    #define loop(i,s,e) for(long long i=s ; i<e ; i++)
+    #define bloop(i, e, s) for(int i=e-1; i>=s; i--)
+    #define ll long long
+    #define str string
+    #define pii pair<int, int>
+    #define pll pair<long long, long long>
+    #define mp make_pair
+    #define f first
+    #define s second
+    #define mapi map<int, int>
+    #define mapll map<long long, long long>
+    #define pb push_back
+    #define pf push_front
+    #define popb pop_back
+    #define popf pop_front
+    #define vi vector<int>
+    #define vll vector<long long>
+    #define li list<int>
+    #define nl "\n"
+     
+    using namespace std;
+    ll t, n;
+    str s;
+    int main() {
+        FAST;
+        cin >> t;
+        while(t--) {
+            cin >> n >> s;
+            bool check = false;
+            int firstInd, lastInd;
+            loop(i, 0, n) {
+                if(s[i] == 'B') {
+                    if(!check) {
+                        firstInd = i;
+                        check = true;
+                    }
+                    lastInd = i;
+                }
+            }
+            cout << lastInd - firstInd +1  << nl;
+            
+        }
+        return 0;
+    }
