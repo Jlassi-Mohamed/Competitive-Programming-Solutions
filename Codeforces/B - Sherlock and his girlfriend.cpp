2440e1b6879769bf266693bc8fe50e9be0a96a77
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2024-09-19 14:12:53 +0100

    Create B - Sherlock and his girlfriend.cpp | C++20 (GCC 13-64) 	Accepted 	62 ms 	100 KB

---
+++ Codeforces/B - Sherlock and his girlfriend.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define GRAPH vector<vector<int>>
+#define loop(i, s, e) for(int i=s ; i<e ; i++)
+#define lloop(i, s, e) for(long long i=s ; i<e ; i++)
+#define bloop(i, e, s) for(int i=e-1; i>=s; i--)
+#define blloop(i, e, s) for(long long i=e-1; i>=s; i--)
+#define ll long long
+#define str string
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mp make_pair
+#define F first
+#define S second
+#define mapi map<int, int>
+#define mapll map<long long, long long>
+#define pb push_back
+#define eb emplace_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define vi vector<int>
+#define vll vector<long long>
+#define li list<int>
+#define nl "\n"
+using namespace std;
+const int MAX = numeric_limits<int>::max();
+const int MIN = numeric_limits<int>::min();
+ll t;
+void solve() {
+    ll n;
+    cin >>n;
+    if(n<=2) {
+        cout << "1" << nl;
+        loop(i, 0, n) {
+            cout << "1" << " ";
+        }
+    }
+    else {
+        cout << "2" << nl; // 2 colors
+        loop(i, 2, n+2) {
+            int check=false;
+            for(int j=2; j*j<=i; j++) {
+                if(i%j==0) {
+                    check = true;
+                    break;
+                }
+            }
+            check ? cout << "2 " : cout << "1 ";
+        }
+    }
+}
+
+int main() {
+    FAST;
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    //cin >> t;
+    t = 1;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
