10a95d7b81e639212ebabbdecac3c5a15066dccb
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2024-09-23 14:27:40 +0100

    Create 2014B - Robin Hood and the Major Oak.cpp | C++20 (GCC 13-64) 	Accepted 	61 ms 	0 KB

---
+++ Codeforces/2014B - Robin Hood and the Major Oak.cpp
@@ -0,0 +1,48 @@
+    #include <bits/stdc++.h>
+    #define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    #define GRAPH vector<vector<int>>
+    #define loop(i, s, e) for(int i=s ; i<e ; i++)
+    #define lloop(i, s, e) for(long long i=s ; i<e ; i++)
+    #define bloop(i, e, s) for(int i=e-1; i>=s; i--)
+    #define blloop(i, e, s) for(long long i=e-1; i>=s; i--)
+    #define ll long long
+    #define str string
+    #define pii pair<int, int>
+    #define pll pair<long long, long long>
+    #define mp make_pair
+    #define F first
+    #define S second
+    #define mapi map<int, int>
+    #define mapll map<long long, long long>
+    #define pb push_back
+    #define eb emplace_back
+    #define pf push_front
+    #define popb pop_back
+    #define popf pop_front
+    #define vi vector<int>
+    #define vll vector<long long>
+    #define li list<int>
+    #define nl "\n"
+    using namespace std;
+    const int MAX = numeric_limits<int>::max();
+    const int MIN = numeric_limits<int>::min();
+    ll t;
+    void solve() {
+        ll n, k;
+        cin >> n >> k;
+        ll res = (n*(n+1)/2)-((n-k+1)*(n-k)/2);
+        res%2 ? cout << "NO" : cout << "YES";
+        cout << nl;
+     
+    }
+     
+    int main() {
+        FAST;
+        //freopen("input.txt", "r", stdin);
+        //freopen("output.txt", "w", stdout);
+        cin >> t;
+        while(t--) {
+            solve();
+        }
+        return 0;
+    }
