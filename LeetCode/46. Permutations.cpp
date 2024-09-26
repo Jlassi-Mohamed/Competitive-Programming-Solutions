7d72b552c0dccbae2b9324c4a2ede2bb2d9036f2
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2024-09-26 10:02:57 +0100

    Runtime 0ms Beats 100.00% | Memory 10.07MB Beats 38.21%

---
+++ LeetCode/46. Permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>& ans, vector<bool>& vis, vector<int>& permutation, vector<int>& nums) {
+        int n = nums.size();
+        if(permutation.size() == n) {
+            ans.push_back(permutation);
+            return ;
+        }
+        for(int i=0; i<n; i++) {
+            if(vis[i])
+                continue;
+            vis[i] = true;
+            permutation.push_back(nums[i]);
+            solve(ans, vis, permutation, nums);
+            // undo
+            permutation.pop_back(); 
+            vis[i] = false;
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<bool> vis(nums.size(), false);
+        vector<vector<int>> ans;
+        vector<int> permutation;
+        solve(ans, vis, permutation, nums);
+        return ans;
+    }
+};
