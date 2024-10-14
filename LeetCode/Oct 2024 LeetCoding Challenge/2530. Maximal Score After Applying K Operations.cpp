5287185b84acc7d72cfb3d6b6a709a793a79b13e
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2024-10-14 19:31:37 +0100

    Runtime 153ms Beats 95.51% | Memory 75.93MB Beats 88.03%

---
+++ LeetCode/Oct 2024 LeetCoding Challenge/2530. Maximal Score After Applying K Operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        long long res=0;
+        for(int i=0; i<k; i++){
+            int x=pq.top();
+            res+=x;
+            if (x==1){
+                res+=(k-1-i);
+                break;
+            }
+            pq.pop();
+            pq.push((x+2)/3);
+        }
+        return res;
+    }
+};
