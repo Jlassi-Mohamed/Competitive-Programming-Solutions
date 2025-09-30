c3b0ed351fe59db92739b35d9daaf96ab0466de7
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2025-09-30 11:22:14 +0100

    C++20 (GCC 13-64) | Accepted | 77 ms | 0 KB

---
+++ Codeforces/B - Unconventional Pairs.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define GRAPH vector<vector<int>>
+#define loop(i, s, e) for(int i=s ; i<e ; i++)
+#define lloop(i, s, e) for(long long i=s ; i<e ; i++)
+#define bloop(i, e, s) for(int i=e-1; i>=s; i--)
+#define blloop(i, e, s) for(long long i=e-1; i>=s; i--)
+#define ll long long
+#define str string
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define mp make_pair
+#define F first
+#define S second
+#define mapi map<int, int>
+#define mapll map<long long, long long>
+#define pb push_back
+#define eb emplace_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define vi vector<int>
+#define vll vector<long long>
+#define li list<int>
+#define nl "\n"
+using namespace std;
+const int MAX = numeric_limits<int>::max();
+const int MIN = numeric_limits<int>::min();
+ll t;
+void solve() {
+    int n; cin >> n;
+    vi v;
+    loop(i, 0, n) {
+        int x; cin >> x;
+        v.pb(x);
+    }
+    sort(v.begin(), v.end());
+    int sz = (int)v.size();
+    int maxDiff = MIN;
+    for(int i=1; i<sz; i+=2) {
+        if((v[i] - v[i-1])>maxDiff) maxDiff = v[i] - v[i-1];
+    }
+    cout << maxDiff << nl;
+}
+
+int main() {
+    FAST;
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
