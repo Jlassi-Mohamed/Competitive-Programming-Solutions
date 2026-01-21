d62069bd26523ee8ccf02a4d457891515431aee4
Jlassi-Mohamed <147339815+Jlassi-Mohamed@users.noreply.github.com>
2026-01-21 19:08:35 +0100

    Runtime 7 ms Beats 19.47% Analyze Complexity Memory 9.85 MB Beats 25.41%

---
+++ LeetCode/Arrays & Hashing/242. Valid Anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        string ns = s;
+        string nt = t;
+
+        sort(ns.begin(), ns.end());
+        sort(nt.begin(), nt.end());
+
+        return ns == nt;
+    }
+};
